73f30287233b7d7a44994ef9c19050350496eb3b
ibnuwildan <ibnuwildann@gmail.com>
2022-03-10 10:13:29 +0700

    change index html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
+// get data json
 $.getJSON("data/card.json", function (data) {
   let menu = data.menu;
   $.each(menu, function (i, data) {
     console.log(data);
     $("#daftar-card").append('<div class="col-3 zoom">' + data.gambar + "</div>");
   });
+});
+
+// navbar active
+$(".nav-link").on("click", function () {
+  $(".nav-link").removeClass("active");
+  $(this).addClass("active");
 });
